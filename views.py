09f3438dc884d19a04960d9307e2cf72a0457d85
Adrienny Thiffanny <165061856+Enny2054@users.noreply.github.com>
2025-04-04 10:39:36 -0300

    views.py

--- views.py
+++ views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
+def home(request):
+    produtos = Produto.object.all()
+    return render(request, 'home.html', {'produtos':produtos})
+
+    def detauls(request, produto_id):
+        produto = get_object_or_404(Produito, pk=produto_id)
+        return render(request, 'details.html', {'produto':produto})
