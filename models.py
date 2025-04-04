96286d2cb8c52721d64f2e304daedb4442a763d2
Adrienny Thiffanny <165061856+Enny2054@users.noreply.github.com>
2025-04-04 10:40:51 -0300

    models.py

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='produtos/')
+
+    def __str__(self):
+        return self.name
+
+        def image_admin(self):
+            if self.image:
+                return mark_safe(f'<img src="{self.image.url}"
+                width="80" />')
+                else:
+                    return "No image"
+
+                    image_admin.short_description = 'Image'
